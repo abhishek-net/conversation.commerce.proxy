5c75678eff483be00028564fbac361431b5be742
Abhishek Mishra <abhisheks.net@hotmail.com>
2017-04-10 21:33:39 +0530

    port change

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ var proxy = httpProxy.createProxyServer({});
 var server = http.createServer(function (req, res) {
     proxy.web(req, res, { target: 'http://sapient5.evaluation.dw.demandware.net/s/SiteGenesisGlobal' });
 });
-server.listen(5050);
+server.listen(process.env.PORT || 5050);
+console.log("Server started on port " + process.env.PORT);
