11d154fe7ec9d3d039bbc945015188d4c8d64a77
Abhishek Mishra <abhisheks.net@hotmail.com>
2017-04-11 12:50:31 +0530

    Url mapping redeined

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ let httpProxy = require('http-proxy');
 
 var proxy = httpProxy.createProxyServer({});
 var server = http.createServer(function (req, res) {
-    proxy.web(req, res, { target: 'http://sapient5.evaluation.dw.demandware.net/s/SiteGenesisGlobal' });
+    proxy.web(req, res, { target: 'http://sapient5.evaluation.dw.demandware.net' });
+});
+///s/SiteGenesisGlobal
+server.on("/on", (req, res) => {
+    proxy.web(req, res, {
+        target: 'http://sapient5.evaluation.dw.demandware.net/on'
+    });
+});
+server.on("/static", (req, res) => {
+    proxy.web(req, res, {
+        target: 'http://sapient5.evaluation.dw.demandware.net/static'
+    });
 });
 server.listen(process.env.PORT || 5050);
 console.log("Server started on port " + process.env.PORT);
